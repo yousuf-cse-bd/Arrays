b0ea74f69185d608325e033f4648e466f93224c9
yousuf-cse-bd <yousuf.cse17@gmail.com>
2025-04-02 23:50:41 +0600

    Array with Enhance for loop (ForLoop-each loop)

---
+++ ArrayBasic.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class ArrayBasic {
+    public static void main(String[] args) {
+        int[] numbers = {11, 22, 33, 44}; // Shorthand initialized array elements
+        for(int number : numbers){ //Enhance for loop (ForLoop-each loop)
+            System.out.println(number);
+        }
+    }
+}
